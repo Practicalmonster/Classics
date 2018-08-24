7388533dffc198bbefaff3b5babca79ed7ce5441
Practical Monster <practicalmonster.itsec@gmail.com>
2018-08-24 11:37:17 +0200

    Designed the GUI

---
+++ Shortest path visualized/src/application/SampleController.java
@@ -0,0 +1,10 @@
+package application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SampleController {
+	@FXML
+	public void dod() {
+//		System.out.println(this.getClass());
+	}
+}
